4680001c18ead61f25bacd7cbf39ebcb26ac6bea
Rusto <Rusto@users.noreply.github.com>
2014-06-16 12:43:53 +0600

    Create background.sh

---
+++ background.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# проверка определенной директории на появление новых файлов
+
+# конвертировать изображения под экран n900
+while ls
+
+convert -sample 800x480 ${2} /home/user/.backgrounds/background-${1}.png
+
+1 /1.jpg
+2 /2.jpg
+3 /3.jpg
+4 /4.jpg
